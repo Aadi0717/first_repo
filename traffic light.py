b40a165cd6185ff8093bd3a307d16b71972f2830
Aadi0717 <aadityajain0987@gmail.com>
2026-09-26 17:26:45 +0530

    add traffic light program

---
+++ traffic light.py
@@ -0,0 +1,10 @@
+light=input("light:")
+if(light=="yellow"):
+    print('start')
+elif(light=="green"):
+    print('go')
+elif(light=="red"):
+    print("stop")
+else:
+    print('traffic light has 3 colour only')
+                                                            
